d932857327f6fab56a14655113b568e0b253dd1b
Jiri Altman <jiri.altman@konicaminolta.cz>
2020-03-15 17:30:51 +0100

    Changed OAUTH_TOKEN to GITHUB_TOKEN

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Website Scrapped at $(date)"
 apt-get update
 apt install wget curl git -y
-curl -H "Authorization: token $OAUTH_TOKEN" https://api.github.com/user/repos -d '{"name":"Website","private":false}'
+curl -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" https://api.github.com/repos -d '{"name":"Website","private":false}'
 wget -r $URL
 cd $URL
 git init
@@ -11,4 +11,4 @@ git config --global user.email "altman.jiri@gmail.com"
 git config --global user.name "$USER"
 git add .
 git commit -m "Final Update"
-git push https://$OAUTH_TOKEN:x-oauth-basic@github.com/$USER/Website.git master --force
+git push https://${{ secrets.GITHUB_TOKEN }}:x-oauth-basic@github.com/$USER/Website.git master --force

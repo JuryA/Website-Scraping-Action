a548e1878b3bc4486e4e84d4d090b6f5e1b95fd6
Jiri Altman <jiri.altman@konicaminolta.cz>
2020-03-16 07:40:55 +0100

    Changed target reop to Website-JuryA-Keybase

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Website Scrapped at $(date)"
 apt-get update
 apt install wget curl git -y
-curl -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" https://api.github.com/repos -d '{"name":"Website","private":false}'
+curl -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" https://api.github.com/repos -d '{"name":"Website-JuryA-Keybase","private":false}'
 wget -r $URL
 cd $URL
 git init
@@ -11,4 +11,4 @@ git config --global user.email "altman.jiri@gmail.com"
 git config --global user.name "$USER"
 git add .
 git commit -m "Final Update"
-git push https://${{ secrets.GITHUB_TOKEN }}:x-oauth-basic@github.com/$USER/Website.git master --force
+git push https://${{ secrets.GITHUB_TOKEN }}:x-oauth-basic@github.com/$USER/Website-JuryA-Keybase.git master --force

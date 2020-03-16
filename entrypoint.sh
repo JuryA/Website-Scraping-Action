d9e719097807a6072c61ba43c0c2e0bde5c1ab23
Jiri Altman <jiri.altman@konicaminolta.cz>
2020-03-16 07:54:36 +0100

    Fix of ${{ ... }} in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo "Website Scrapped at $(date)"
 apt-get update
 apt install wget curl git -y
-curl -H "Authorization: Bearer ${{ github.token }}" https://api.github.com/repos -d '{"name":"Website-JuryA-Keybase","private":false}'
+curl -H "Authorization: Bearer $TOKEN" https://api.github.com/repos -d '{"name":"Website-JuryA-Keybase","private":false}'
 wget -r $URL
 cd $URL
 git init
@@ -11,4 +11,4 @@ git config --global user.email "altman.jiri@gmail.com"
 git config --global user.name "$USER"
 git add .
 git commit -m "Final Update"
-git push https://${{ github.token }}:x-oauth-basic@github.com/$USER/Website-JuryA-Keybase.git master --force
+git push https://$TOKEN:x-oauth-basic@github.com/$USER/Website-JuryA-Keybase.git master --force
